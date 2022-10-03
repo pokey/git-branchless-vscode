4c79e1e0ec71b3fbe994b09fce567b207620ba02
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-10-03 17:29:04 +0100

    Cleanup
    Fixes #2

---
+++ src/parseOrDisplayError.ts
@@ -0,0 +1,29 @@
+import { toPairs } from "lodash";
+import * as vscode from "vscode";
+import { z, ZodError } from "zod";
+
+export function parseOrDisplayError<T>(
+  schema: z.ZodType<T>,
+  value: unknown
+): T {
+  try {
+    return schema.parse(value);
+  } catch (e) {
+    const error = e as ZodError;
+
+    const { formErrors, fieldErrors } = error.flatten();
+    formErrors.forEach((message) => {
+      vscode.window.showErrorMessage(message);
+    });
+    toPairs(fieldErrors).forEach(([key, messages]) => {
+      if (messages == null) {
+        return;
+      }
+
+      messages.forEach((message) => {
+        vscode.window.showErrorMessage(`Error on key '${key}': ${message}`);
+      });
+    });
+    throw error;
+  }
+}
--- src/registerCommands.ts
+++ src/registerCommands.ts
@@ -2,32 +2,43 @@ import * as vscode from "vscode";
 import { z } from "zod";
 import { CommandArgDescription } from "./CommandDescription.types";
 import { commands } from "./commands/commands";
+import { parseOrDisplayError } from "./parseOrDisplayError";
 
 let terminal: vscode.Terminal | undefined = undefined;
 
 function constructArgSchema(args: CommandArgDescription[]) {
   return z
-    .object(
-      Object.fromEntries(args.map(({ key }) => [key, z.string().optional()]))
-    )
+    .object({
+      args: constructArgListSchema(args).optional(),
+      noConfirmation: z.boolean().optional(),
+    })
     .strict()
     .optional();
 }
 
+function constructArgListSchema(args: CommandArgDescription[]) {
+  return z
+    .object(
+      Object.fromEntries(args.map(({ key }) => [key, z.string().optional()]))
+    )
+    .strict();
+}
+
 export function registerCommands(context: vscode.ExtensionContext) {
-  const disposables = commands.map(({ id, command, args }) =>
+  const disposables = commands.map(({ id, command, args: expectedArgs }) =>
     vscode.commands.registerCommand(
       `git-branchless.${id}`,
       async (rawArg?: unknown) => {
-        const arg = constructArgSchema(args).parse(rawArg) ?? {};
+        const { args: actualArgs = {}, noConfirmation = false } =
+          parseOrDisplayError(constructArgSchema(expectedArgs), rawArg) ?? {};
 
         if (terminal == null) {
           terminal = vscode.window.createTerminal({ isTransient: true });
         }
 
         const commandArgs = [];
-        for (const { key, flag, description } of args) {
-          let value = arg[key];
+        for (const { key, flag, description } of expectedArgs) {
+          let value = actualArgs[key];
           if (value == null) {
             // FIXME: Use quick pick with commits or whatever depending on arg type
             value = await vscode.window.showInputBox({ prompt: description });
@@ -43,7 +54,7 @@ export function registerCommands(context: vscode.ExtensionContext) {
 
         terminal.sendText(
           `git-branchless ${command} ${commandArgString} && git-branchless smartLog`,
-          false
+          noConfirmation
         );
         terminal.show();
       }
